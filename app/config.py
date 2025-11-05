54d1ef1adf172b9817487b2302702ffab767b8de
Emile679 <emile.debourdeaudhuy@ugent.be>
2025-11-05 15:28:28 +0100

    config

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-class Config: 
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.username:password@localhost:port/dbname'
+class Config:
+    SECRET_KEY = 'cuvwiN-zosmob-8wonzu'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:cuvwiN-zosmob-8wonzu@db.pavocfhmigmdzrzxoiio.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SECRET_KEY = 'cuvwiN-zosmob-8wonzu'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:cuvwiN-zosmob-8wonzu@db.pavocfhmigmdzrzxoiio.supabase.co:5432/postgres'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
